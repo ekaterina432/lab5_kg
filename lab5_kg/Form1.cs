6aaad131e99cd88b3eb0527f7092d584c26a9ad0
KatePrish <prishchepa@sfedu.ru>
2023-10-17 22:51:10 +0300

    Добавьте файлы проекта.

---
+++ lab5_kg/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5_kg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form_fraktal frm = new form_fraktal();
+            frm.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
