adde873ccb0df9595320c6bbf247fc802e0e2e15
KatePrish <prishchepa@sfedu.ru>
2023-10-18 00:02:21 +0300

    Считывание файлов L-систем

--- lab5_kg/form_fraktal.Designer.cs
+++ lab5_kg/form_fraktal.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Open = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(42, 47);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(128, 45);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "Выбери L-систему";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(617, 381);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // form_fraktal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "form_fraktal";
+            this.ClientSize = new System.Drawing.Size(1014, 578);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Open);
+            this.Name = "form_fraktal";
+            this.Text = "Фракталы";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- lab5_kg/form_fraktal.cs
+++ lab5_kg/form_fraktal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,62 @@ namespace lab5_kg
 {
     public partial class form_fraktal : Form
     {
+        Graphics g;
+        string axiom;
+        double angle;
+        string direction;
+        int iterations;
+        SortedDictionary<char, string> rules;
+        Stack<Tuple<double, double, double, double>> savedStates;
+        double re, gree, wi;
+        Random rand = new Random();
+
+
         public form_fraktal()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            rules = new SortedDictionary<char, string>();
+            savedStates = new Stack<Tuple<double, double, double, double>>();
+
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Text files|*.TXT";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fname = openDialog.FileName;
+                    string[] flines = File.ReadAllLines(fname);
+                    string[] parameters = flines[0].Split(' ');
+
+                    axiom = parameters[0];
+                    angle = Convert.ToDouble(parameters[1]);
+                    direction = parameters[2];
+
+                    rules.Clear();
+                    string[] rule;
+                    for (int i = 1; i < flines.Length; ++i)
+                    {
+                        rule = flines[i].Split('>');
+                        rules[Convert.ToChar(rule[0])] = rule[1];
+                    }
+                }
+                catch
+                {
+                    DialogResult result = MessageBox.Show("Не удается открыть файл.",
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
     }
 }
